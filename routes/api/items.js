6fda337a82c9ba82e356d1dd32edd67669481cb5
Gerald Bullard Jr <geraldbullard@purplerockscissors.com>
2022-06-09 17:38:30 -0400

    GET, POST, PUT, DELETE all working now with db.

--- routes/api/items.js
+++ routes/api/items.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Item = require('../../models/Item');
 
 // @route GET api/items
-// @desc Get All Items
+// @desc Get all Items
 // @access Public
 router.get('/', (req, res) => {
     Item.find()
@@ -20,7 +20,30 @@ router.post('/', (req, res) => {
     const newItem = new Item({
         name: req.body.name,
     });
-    newItem.save().then(item => res.json(item));
+    newItem
+        .save()
+        .then(item => res.json(item))
+        .catch(err => res.status(404).json({ error: err }));
+});
+
+// @route PUT api/items/:id
+// @desc Update an Item
+// @access Public
+router.put('/:id', (req, res) => {
+    Item
+        .findByIdAndUpdate(req.params.id, req.body)
+        .then(() => res.json({ success: true }))
+        .catch(err => res.status(404).json({ error: err }));
+});
+
+// @route DELETE api/items/:id
+// @desc Delete an Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item
+        .findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ error: err }));
 });
 
 module.exports = router;
