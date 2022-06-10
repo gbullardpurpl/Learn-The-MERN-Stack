0bfa7e06f37a910af95652f63487bd690b414712
Gerald Bullard Jr <geraldbullard@purplerockscissors.com>
2022-06-10 11:58:21 -0400

    working navbar w Bootstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false,
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        });
+    }
+    render() {
+        return (
+            <Navbar color="dark" dark expand="sm" class="mb-5">
+                <NavbarBrand href="/">Shopping List</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="https://github.com/gbullardpurpl/Learn-The-MERN-Stack" target="_blank">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option
+                                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default AppNavbar;
