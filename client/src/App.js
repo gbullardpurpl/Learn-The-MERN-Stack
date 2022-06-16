3675eb78e02dfc799a86ca0aaff4b7e1498774ac
Gerald Bullard Jr <geraldbullard@purplerockscissors.com>
2022-06-16 18:04:49 -0400

    P5 Complete.

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import AppFooter from './components/AppFooter';
 
-//import { Provider } from 'react-redux';
-//import store from '../store';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -17,6 +17,7 @@ class App extends Component {
         * Removed StrictMode from wrapping entire App due to findDOMNode deprecation
         * until a better solution is found to actually fix the issue, GBJ 06-10-22
         */
+      <Provider store={store}>
         <div className="App">
           <React.Fragment>
             <AppNavbar />
@@ -24,6 +25,7 @@ class App extends Component {
             <AppFooter />
           </React.Fragment>
         </div>
+      </Provider>
     );
   }
 }
